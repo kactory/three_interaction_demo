f6b2ad1ea8faef7e2ed841245f674c561eb423a1
keykat <koin12000@naver.com>
2025-02-09 18:15:17 +0900

    [feauture] wall 리소스 여러개 한꺼번에 import 해보기

--- app/components/contants/models.tsx
+++ app/components/contants/models.tsx
@@ -1,21 +1,28 @@
 export const MODEL_PATHS = {
     CHARACTERS: {
-
+        bear: { path: '/models/characters/bear.gltf' },
+        dog: { path: '/models/characters/dog.gltf' },
+        duck: { path: '/models/characters/duck.gltf' },
+        witch: { path: '/models/characters/witch.gltf' },
     },
     ENVIRONMENT: {
 
     },
     OBJECTS: {
-        tables: {
-            path: '/models/objects/table.gltf',
-            type: 'GLTF',
-            scale: 1,
-            position: {
-                x: 0,
-                y: 0,
-                z: 0
-            }
-        }
+        tables: { path: '/models/objects/table.gltf' },
+        barrel: { path: '/models/objects/barrel.gltf' },
+        bench: { path: '/models/objects/bench.gltf' },
+        bookcase_broken: { path: '/models/objects/bookcase_broken.gltf' },
+        bookcase_wide_broken: { path: '/models/objects/bookcase_wide_broken.gltf' },
+        bookcase_wide: { path: '/models/objects/bookcase_wide.gltf' },
+        bookcase: { path: '/models/objects/bookcase.gltf' },
+        bricks: { path: '/models/objects/bricks.gltf' },
+        broken_wall: { path: '/models/objects/broken_wall.gltf' },
+        chair: { path: '/models/objects/chair.gltf' },
+        crate: { path: '/models/objects/crate.gltf' },
+        door: { path: '/models/objects/door.gltf' },
+        open_book: { path: '/models/objects/open_book.gltf' },
+        wall: { path: '/models/objects/wall.gltf' },
     },
     PROPS: {
 
